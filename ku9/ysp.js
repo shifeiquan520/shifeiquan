402f58947a907514487e516002840bb722702daf
shifeiquan520 <shifeiquan@qq.com>
2026-06-08 12:54:44 +0800

    Update ysp.js

--- ku9/ysp.js
+++ ku9/ysp.js
@@ -1,35 +1,4 @@
 function main(item) {
-    var urlStr = item.url || '';
-    var id = item.id || '';
-    if (!id) {
-        var qm = urlStr.indexOf('?');
-        if (qm >= 0) {
-            var qs = urlStr.substring(qm + 1).split('&');
-            for (var i = 0; i < qs.length; i++) {
-                var p = qs[i].split('=');
-                if (p[0] === 'id' && p.length > 1) id = p[1];
-            }
-        }
-    }
-    if (!id) id = 'cctv1';
-
-    var playseek = item.playseek || '';
-    if (!playseek) {
-        var qm = urlStr.indexOf('?');
-        if (qm >= 0) {
-            var qs = urlStr.substring(qm + 1).split('&');
-            for (var i = 0; i < qs.length; i++) {
-                var p = qs[i].split('=');
-                if (p[0] === 'playseek' && p.length > 1) playseek = p[1];
-            }
-        }
-    }
-
-    var phpUrl = urlStr.replace(/\.js/i, '.php');
-    if (playseek) {
-        phpUrl += (phpUrl.indexOf('?') >= 0 ? '&' : '?') + 'playseek=' + playseek;
-    }
-
-    return { url: phpUrl, headers: { 'User-Agent': 'qqlive', 'Referer': 'https://tv.cctv.com/' } };
+    return { url: 'http://43.136.81.155:8888/641886683', headers: { 'User-Agent': 'qqlive' } };
 }
 if (typeof module !== 'undefined' && module.exports) { module.exports = { main: main }; }
